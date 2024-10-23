71891ac6ea6b56dff753f4e1acde118ca3a3bafd
Gizem-Bilgic <gizemmbilgicc@gmail.com>
2024-10-23 17:23:05 +0300

    Add project files.

---
+++ 01.MVCIntro/Controllers/UrunController.cs
@@ -0,0 +1,27 @@
+﻿using _01.MVCIntro.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _01.MVCIntro.Controllers
+{
+    public class UrunController : Controller
+    {
+        private static List<Urun> urunler = new List<Urun>
+        { 
+            new Urun { Id = 1, Ad = "Ürün 1", Fiyat = 100},
+            new Urun { Id = 2, Ad = "Ürün 2", Fiyat = 200},
+            new Urun { Id = 3, Ad = "Ürün 3", Fiyat = 300}
+        };
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Liste()
+        {
+            return View(urunler);
+        }
+        public IActionResult Table()
+        {
+            return View(urunler);
+        }
+    }
+}
